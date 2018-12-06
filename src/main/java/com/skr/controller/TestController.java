f600d3d702c53b59fc6d65d32630f13620cd876c
seasonscar <525744150@qq.com>
2018-12-06 23:47:37 +0800

    后台

--- src/main/java/com/skr/controller/TestController.java
+++ src/main/java/com/skr/controller/TestController.java
@@ -17,30 +17,52 @@ import javax.servlet.http.HttpServletRequest;
  * Created by 15082188 on 2018/8/2.
  */
 
-
+@Controller
 public class TestController {
-    private static final Log log= LogFactory.getLog(TestController.class);
+    private static final Log log = LogFactory.getLog(TestController.class);
     @Autowired
     TestService testService;
+
     @ResponseBody
-    @RequestMapping(value="test.action",method = RequestMethod.GET)
-    public String testHttpRequest(){
-        String a=testService.queryForString();
+    @RequestMapping(value = "test.action", method = RequestMethod.GET)
+    public String testHttpRequest() {
+        String a = testService.queryForString();
         log.error("this is a error");
         log.info("this is a test_log");
         return a;
     }
 
-    @RequestMapping(value="testMoney.action",method = RequestMethod.GET)
-    public String testMoney(HttpServletRequest request){
+    @RequestMapping(value = "testMoney.htm", method = RequestMethod.GET)
+    public String testMoney(HttpServletRequest request) {
         String a=testService.queryForString();
         log.error("this is a error");
         log.info("this is a test_log");
-        request.setAttribute("lkm","李康民");
-        return "x-money/moneyIndex.ftl";
+        request.setAttribute("lkm", "李康民");
+        request.setAttribute("ip",getIpAddress(request));
+        return "catHomeIndex";
     }
 
-    static void print(){
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    static void print() {
         System.out.println("I AM STATIC");
     }
 }
